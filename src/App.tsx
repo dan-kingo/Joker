f0715b13575b42a118c882750225df051e689fff
Dan Kingo <danieldejen23@gmail.com>
2024-10-03 22:51:01 +0300

    add responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+
 const App = () => {
   return (
     <div>
-      <Button colorScheme="blue"> Button</Button>
+      <Grid
+        templateAreas={{
+          base: '"nav" "main"',
+          lg: `"nav nav"
+                        "side main"`,
+        }}
+        gap="2"
+      >
+        <GridItem area="nav" bg="tomato">
+          Header
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="side" bg="blue.100">
+            Side Navigation
+          </GridItem>
+        </Show>
+
+        <GridItem area="main" bg="blue.300">
+          Main Content
+        </GridItem>
+      </Grid>
     </div>
   );
 };
