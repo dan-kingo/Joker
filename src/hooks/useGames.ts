623e9de0dee45a35b37cb6a6e07834bb1607a690
Dan Kingo <danieldejen23@gmail.com>
2024-10-05 13:10:20 +0300

    fix error in the filter logic

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -15,7 +15,7 @@ export interface Game {
 }
 
 const useGames = (selectedGenre: Genre | null) =>
-  useData<Game>("/games", { params: { genre: selectedGenre?.id } }, [
+  useData<Game>("/games", { params: { genres: selectedGenre?.id } }, [
     selectedGenre?.id,
   ]);
 export default useGames;
