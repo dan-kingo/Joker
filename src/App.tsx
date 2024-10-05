aeab606096f7df90f448fa1d344c33ddb6030594
Dan Kingo <danieldejen23@gmail.com>
2024-10-05 10:16:46 +0300

    implment drawer in our app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react";
+import {
+  Grid,
+  GridItem,
+  Show,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  DrawerBody,
+  useDisclosure,
+} from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
 
 const App = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <div>
       <Grid
@@ -16,10 +28,10 @@ const App = () => {
           base: "1fr",
           lg: "250px 1fr",
         }}
-        gap="2"
+        gap={2}
       >
-        <GridItem area="nav">
-          <NavBar />
+        <GridItem area="nav" bg="blackAlpha.900">
+          <NavBar onOpen={onOpen} />
         </GridItem>
         <Show above="lg">
           <GridItem paddingX={4} area="side">
@@ -31,6 +43,16 @@ const App = () => {
           <GameGrid />
         </GridItem>
       </Grid>
+
+      <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerBody>
+            <GenreList /> {/* Genre list inside the drawer */}
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
     </div>
   );
 };
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,25 @@
-import { HStack, Image } from "@chakra-ui/react";
+import { HStack, Image, Button } from "@chakra-ui/react";
 import logo from "../assets/joker-high-resolution-logo-white-transparent.png";
 import ToogleColorMode from "./ToogleColorMode";
-const NavBar = () => {
+import { FaBars } from "react-icons/fa";
+
+interface Props {
+  onOpen: () => void;
+}
+const NavBar = ({ onOpen }: Props) => {
   return (
-    <>
-      <HStack padding={4} justifyContent="space-between">
-        <Image src={logo} width="90px" bg="#1A202C" />
-        <ToogleColorMode />
-      </HStack>
-    </>
+    <HStack padding={4} justifyContent="space-between">
+      <Button
+        aria-label="Open Menu"
+        onClick={onOpen}
+        display={{ base: "block", lg: "none" }}
+      >
+        {<FaBars />}
+      </Button>
+      <Image src={logo} width="90px" />
+
+      <ToogleColorMode />
+    </HStack>
   );
 };
 
