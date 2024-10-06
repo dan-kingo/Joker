f552a7eb45bcbd736585619fc002dee2945612c5
Dan Kingo <danieldejen23@gmail.com>
2024-10-06 12:10:45 +0300

    handling game without image

--- src/services/getCroppedImageUrl.ts
+++ src/services/getCroppedImageUrl.ts
@@ -1,7 +1,9 @@
+import noImage from "../assets/no-image.jpg";
+
 const getCroppedImageUrl = (url: string) => {
   const target = "media/";
 
-  if (url === null) return "";
+  if (url === null) return noImage;
   const index = url.lastIndexOf(target) + target.length;
 
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
